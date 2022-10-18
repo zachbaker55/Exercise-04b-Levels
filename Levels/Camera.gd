5b3c277da188d6b7c9c9d68cc05786bd87486bf6
Jason Francis <111897544+BL-MSCH-C220-F22@users.noreply.github.com>
2022-10-18 14:26:51 -0400

    Initial commit

---
+++ Levels/Camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var player = null
+
+func _ready():
+	Global.death_zone = limit_bottom + 200
+
+func _process(_delta):
+	player = get_node_or_null("/root/Game/Player_Container/Player")
+	if player != null:
+		position = player.position
+
+func _physics_process(_delta):
+	var vtrans = get_canvas_transform()
+	var top_left = -vtrans.get_origin() / vtrans.get_scale()
+	$Fade.rect_global_position = top_left
